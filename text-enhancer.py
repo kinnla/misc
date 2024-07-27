b356ba93b7ea64299384f85fda13513f27946e05
kinnla <kinnla@gmail.com>
2024-07-27 10:24:57 +0200

    prompt engineering

--- text-enhancer.py
+++ text-enhancer.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!~/venvs/default/bin/python3
 # coding: utf-8
 
 """
-This script takes a potentially unstructured text file as input, analyzes it and generates
+This script takes a potentially unstructured text file as input, analyzes it, and generates
 a structured version in markdown.
 
 Usage:
@@ -17,6 +17,14 @@ import os
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 MODEL = "gpt-4o-mini"  # Specify the model you want to use
+PROMPT = """Du bist eine Stenographin und Sekretärin und hervorragend darin, Texte \
+mitzuschreiben und zu formulieren. Anbei ein Transkript einer Sprachnotiz, die ich \
+gerade eingesprochen habe. Korrigiere Grammatik und Rechtschreibung, und forme den \
+Text so um, dass er Sinn ergibt. Achte darauf, dass kein Inhalt verloren geht und \
+der Gedankengang erhalten bleibt. Es geht nicht um eine Zusammenfassung. Strukturiere \
+den Text in geeignete Abschnitte und finde Zwischenüberschriften. Nutze Markdown \
+als Ausgabeformat. Gib den Text dann aus, so dass ich ihn zu Protokoll geben kann."""
+
 
 def enhance_text(file_path):
     # Read the input text file
@@ -27,7 +35,7 @@ def enhance_text(file_path):
     response = openai.ChatCompletion.create(
         model=MODEL,
         messages=[
-            {"role": "system", "content": """You are generating a transcript summary. Create a summary of the provided transcription. Respond in Markdown."""},
+            {"role": "system", "content": PROMPT},
             {"role": "user", "content": raw_text}
         ],
         temperature=0
